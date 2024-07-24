68e6bf829cb51d34b7f3d8b85c7edcfc40db79d0
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-24 00:55:59 -0300

    Add test to validate password

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -15,3 +15,11 @@ func TestNewUser(t *testing.T) {
 	assert.Equal(t, "John Doe", user.Name)
 	assert.Equal(t, "j@j.com", user.Email)
 }
+
+func TestUser_ValidatePassword(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "123456")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123456"))
+	assert.False(t, user.ValidatePassword("1234567"))
+	assert.NotEqual(t, "123456", user.Password)
+}
