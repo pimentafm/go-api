5c498933e1bfc3e828253c9a4a6a607dd5727939
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-24 00:53:04 -0300

    Add test to user entity

---
+++ internal/entity/user_test.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "password")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "John Doe", user.Name)
+	assert.Equal(t, "j@j.com", user.Email)
+}
